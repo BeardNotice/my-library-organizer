f67867b21e8f3e22dba4360bf105194beb148142
Ian Flurkey <ianflurkey@gmail.com>
2025-03-14 13:11:48 -0400

    added Library model and 1:M relationship between user and library

--- server/config.py
+++ server/config.py
@@ -7,11 +7,14 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
 
 # Local imports
+import os
 
 # Instantiate app, set attributes
 app = Flask(__name__)
+app.secret_key = b'cffccf6faa164a4896ad0b2efa28fe7d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -21,8 +24,10 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 db = SQLAlchemy(metadata=metadata)
-migrate = Migrate(app, db)
+migrate = Migrate(app=app, db=db)
 db.init_app(app)
+
+bcrypt = Bcrypt(app)
 
 # Instantiate REST API
 api = Api(app)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,31 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    
+    libraries = db.relationship("Library", back_populates="user", cascade="all, delete-orphan")
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+class Library(db.Model, SerializerMixin):
+    __tablename__ = "libraries"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    
+    user = db.relationship("User", back_populates="libraries")
