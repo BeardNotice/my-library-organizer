bf56ee0f304eec6f274a2bb982c7f5070d3bab17
Ian Flurkey <ianflurkey@gmail.com>
2025-04-03 18:22:44 -0400

    updated run build

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -46,7 +46,7 @@ function BookCard({ book, onRate, onDelete, libraryId, allowDelete = true }) {
 
   const handleDelete = () => {
     if (window.confirm("Are you sure you want to delete this book?")) {
-      fetch(`/library/${libraryId}/books/${book.id}`, {
+      fetch(`/api/library/${libraryId}/books/${book.id}`, {
         method: 'DELETE',
         credentials: 'include'
       })
